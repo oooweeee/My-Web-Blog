f061b402ab3beeff044db5889aa916c4ae140064
walker <spadonival7@gmail.com>
2024-05-10 21:05:37 -0400

    added event listner and save function

--- assets/css/js/form.js
+++ assets/css/js/form.js
@@ -1,26 +1,32 @@
-function saveBlog(e){
-e.preventDefault();
-    // THIS IS GETTING THE ARRAY AND PARSING IT 
-console.log("anything")
-    // ("blogs") is a key 
-  let blogs=localStorage.getItem("blogs")
+
+const submit= document.querySelector
+("#blogForm")
+// THIS IS THE FUNCTION THAT WILL SAVE THE BLOG
+submit.addEventListener('submit' , saveBlog)
+function saveBlog(e) {
+  e.preventDefault();
+  // THIS IS PREVENTING THE PAGE FROM REFRESHING 
+  // THIS IS GETTING THE ARRAY AND PARSING IT
+  console.log("anything");
+  // ("blogs") is a key
+  let blogs = localStorage.getItem("blogs");
   // THIS IS INSURING THAT THERE IS NO EMPTY VALUE
-  let parseBlogs=JSON.parse(blogs)||[]
- 
-  // GRABING THE USERS INPUTS
-  const username = document.getElementById('username').value;
-    const blogTitle = document.getElementById('blogTitle').value;
-    const blogContent = document.getElementById('blogContent').value;
-  
-    // THIS WILL REFORMAT IT INTO THE WAY THE BLOG IS SET UP
-     const blogPost = {
-      username,
-      blogTitle,
-      blogContent
-     }
-    //  THIS WILL PUSH blogPost INTO parseBlog or this will take the new information and add it to old info
-  parseBlogs.push(blogPost)
-    
-  // this is parsing the information backe into the aray val word this is taking the new information and putting it where it belongs or // from an array to a string from a string to local storage
-  localStorage.setItem("blogs",JSON.stringify(parseBlogs))   
+  let parseBlogs = JSON.parse(blogs) || [];
+
+  // THIS IS GRABBING THE USERS INPUTS
+  const username = document.getElementById("username").value;
+  const blogTitle = document.getElementById("blogTitle").value;
+  const blogContent = document.getElementById("blogContent").value;
+
+  // THIS WILL REFORMAT IT INTO THE WAY THE BLOG IS SET UP
+  const blogPost = {
+    username,
+    blogTitle,
+    blogContent,
   };
+  //  THIS WILL PUSH blogPost INTO parseBlog or this will take the new information and add it to old info
+  parseBlogs.push(blogPost);
+
+  // this is parsing the information back into the array HEY VAL THIS IS TAKING THE NEW INFORMATION AND PUTTING IT WHERE IT BELONGS OR YOU COULD ALSO SAY //FROM AN ARRAY TO A STRING FROM A STRING TO LOCAL STORAGE
+  localStorage.setItem("blogs", JSON.stringify(parseBlogs));
+}
--- assets/css/js/logic.js
+++ assets/css/js/logic.js
@@ -1,13 +1,11 @@
 // logic.js
 
 //THIS IS HOW I WILL TOGGLE MY DARK MODE
-// document.querySelector('.toggle-button').addEventListener('click', () => {
-//     document.body.classList.toggle('dark-mode');
-//   });
-  
-//   // 
-//   document.querySelector('.back-button').addEventListener('click', () => {
-//     window.location.href = '/index.html';
-//      // 
-//   });
-  
+// document.querySelector(".toggle-button").addEventListener("click", () => {
+//   document.body.classList.toggle("dark-mode");
+// });
+
+// document.querySelector(".back-button").addEventListener("click", () => {
+//   window.location.href = "/index.html";
+// });
+ -
